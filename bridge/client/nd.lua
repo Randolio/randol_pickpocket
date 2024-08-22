5b0265aa399cb454c6c53ea981337e2c4dbf82fe
Torch ICO <tommyrpuk@gmail.com>
2024-08-22 01:26:07 +0100

    Added ND Core compat via bridge.

--- bridge/client/nd.lua
+++ bridge/client/nd.lua
@@ -1,1 +1,33 @@
+if GetResourceState('ND_Core') ~= 'started' then return end
 
+local NDCore = exports.ND_Core
+local PlayerData = {}
+
+AddEventHandler("ND:characterLoaded", function(character)
+    PlayerData = character
+end)
+
+AddEventHandler("ND:characterUnloaded", function(character)
+    table.wipe(PlayerData)
+end)
+
+function hasPlyLoaded()
+    return LocalPlayer.state.isLoggedIn
+end
+
+function DoNotification(text, nType)
+    lib.notify({description = text, type = nType})
+end
+
+function GetPlayerJob()
+    return PlayerData.job
+end
+
+AddEventHandler("ND:updateCharacter", function(character)
+    PlayerData = character
+end)
+
+AddEventHandler('onResourceStart', function(res)
+    if GetCurrentResourceName() ~= res or not hasPlyLoaded() then return end
+    PlayerData = NDCore:getPlayer()
+end)
--- bridge/server/nd.lua
+++ bridge/server/nd.lua
@@ -1,1 +1,48 @@
+if GetResourceState('ND_Core') ~= 'started' then return end
+
+local NDCore = exports["ND_Core"] or exports.ND_Core
+
+function GetPlayer(id)
+    return NDCore:getPlayer(id)
+end
+
+function DoNotification(src, text, nType)
+    TriggerClientEvent('ox_lib:notify', src, {description = text, type = nType})
+end
+
+function GetPlyIdentifier(Player)
+    return Player.id
+end
+
+function GetByIdentifier(cid)
+    return false
+end
+
+function GetSourceFromIdentifier(cid)
+    return false
+end
+
+function GetPlayerSource(Player)
+    return Player.source
+end
+
+function GetCharacterName(Player)
+    return Player.firstname.. ' ' ..Player.lastname
+end
+
+function AddMoney(player, acc, amount)
+    player.addMoney(acc, amount, 'pickpocket')
+end
+
+function AddItem(src, item, amount)
+    exports.ox_inventory:AddItem(src, item, amount)
+end
+
+function itemLabel(item)
+    return exports.ox_inventory:Items(item).label
+end
+
+function itemLabel(item)
+    return exports.ox_inventory:Items(item) and exports.ox_inventory:Items(item).label or ('UNREGISTERED REWARD ITEM: %s'):format(item)
+end
 
