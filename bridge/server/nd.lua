faf86e59e7a7b212eafac2180631e56ed0c36173
ParuThePro <35810485+paruthepro@users.noreply.github.com>
2024-08-22 11:30:43 +0100

    Update nd.lua
    Remove duplicate itemlabel func

--- bridge/server/nd.lua
+++ bridge/server/nd.lua
@@ -44,7 +44,3 @@ function itemLabel(item)
     return exports.ox_inventory:Items(item).label
 end
 
-function itemLabel(item)
-    return exports.ox_inventory:Items(item) and exports.ox_inventory:Items(item).label or ('UNREGISTERED REWARD ITEM: %s'):format(item)
-end
-
